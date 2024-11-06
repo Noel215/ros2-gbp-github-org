5af652ce9a7cced1c4f0d97b04403d7355ad8917
Noel Jimenez <noel.jimenez.gar@gmail.com>
2024-11-06 17:40:29 +0100

    Move teleop_tools to new teleop_team

---
+++ teleop.tf
@@ -0,0 +1,16 @@
+locals {
+  teleop_team = [
+    "bmagyar",
+  ]
+  teleop_repositories = [
+    "teleop_tools-release",
+  ]
+}
+
+module "teleop_team" {
+  source       = "./modules/release_team"
+  team_name    = "teleop"
+  members      = local.teleop_team
+  repositories = local.teleop_repositories
+  depends_on   = [github_membership.members, github_repository.repositories]
+}
